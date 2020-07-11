defa942e0f17169c4310638f93a71d9350bbcbf4
mhuy10 <67315348+mhuy10@users.noreply.github.com>
2020-07-11 13:44:49 +0700

    Add files via upload

---
+++ Server/ServerForm.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Server
+{
+    partial class ServerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ServerForm));
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.BackColor = System.Drawing.Color.White;
+            this.txtDisplay.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtDisplay.Location = new System.Drawing.Point(71, 69);
+            this.txtDisplay.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.ReadOnly = true;
+            this.txtDisplay.Size = new System.Drawing.Size(375, 244);
+            this.txtDisplay.TabIndex = 0;
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Location = new System.Drawing.Point(211, 321);
+            this.btnQuit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(100, 28);
+            this.btnQuit.TabIndex = 1;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(526, 435);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtDisplay);
+            this.panel1.Controls.Add(this.btnQuit);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(6, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(529, 438);
+            this.panel1.TabIndex = 3;
+            // 
+            // ServerForm
+            // 
+            this.AcceptButton = this.btnQuit;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 452);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "ServerForm";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.ServerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Server/ServerForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Server;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Serialization.Formatters;
+using System.Runtime.Remoting.Channels.Http;
+using System.Runtime.Remoting;
+using System.Collections;
+using System.Net;
+using General;
+
+namespace Server
+{
+    public partial class ServerForm : Form
+    {
+        private Bitmap bmpbackg = new Bitmap("..//..//backg.jpg");
+        private int Port = 6123;
+        private Hashtable OnlineUserList; //bảng băm
+        public int i = 0;
+        public  string[] Usenames = new string[100];
+        public ServerForm()
+        {
+            InitializeComponent();
+            
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Image = bmpbackg;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            String hostName = Dns.GetHostName();// lấy địa chỉ IP đã có
+            BinaryClientFormatterSinkProvider clientProvider = new BinaryClientFormatterSinkProvider();//tạo máy khách dùng BinaryFormatter để truyền hông điệp
+            BinaryServerFormatterSinkProvider serverProvider = new BinaryServerFormatterSinkProvider();
+            serverProvider.TypeFilterLevel = System.Runtime.Serialization.Formatters.TypeFilterLevel.Full;//bộ lọc
+            IDictionary props = new Hashtable();//từ điển
+            props["port"] = Port;
+            props["typeFilterLevel"] = TypeFilterLevel.Full;
+
+            HttpChannel chan = new HttpChannel(props, clientProvider, serverProvider);            
+            ChannelServices.RegisterChannel(chan, false);
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(RemoteObject), "RemoteObject.soap", WellKnownObjectMode.SingleCall);
+
+            AddTextToDisplay("Đang mở Port  " + Port + "..." + hostName);
+            OnlineUserList = new Hashtable();
+        }
+
+        delegate void AddTextToDisplayDelegate(String Info);
+        public void AddTextToDisplay(String Info)
+        {
+            if (this.InvokeRequired)
+            {
+                AddTextToDisplayDelegate d = new AddTextToDisplayDelegate(AddTextToDisplay);
+                this.Invoke(d, Info);
+            }
+            else
+            {
+                this.txtDisplay.AppendText(Environment.NewLine + "#" + Info);
+            }
+            
+        }
+
+        internal void AddUserToOnlineUserList(String Username, WrapperTransporterClass wtc)
+        {     
+                OnlineUserList.Add(Username, wtc);
+        }
+
+        internal void ModifyUserOnlineListWrapper(String Username, WrapperTransporterClass wtc)
+        {
+            OnlineUserList[Username] = wtc;
+        }
+
+        internal WrapperTransporterClass GetConnectionTo(String Username)
+        {
+            return (WrapperTransporterClass)OnlineUserList[Username];
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public void SetUsername(string name)
+        {
+            Usenames[i]=name;
+            i++;
+        }
+        public string GetUsername(int i)
+        {
+            return Usenames[i];
+        }
+
+        private void ServerForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
